77ad4bb1d157eb39f52125e06ecab02b9e323616
Andri Firman Saputra <andrifirmansaputra1@gmail.com>
2023-05-22 20:00:41 +0700

    merged

---
+++ paracetamol.php
@@ -0,0 +1,281 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+   <!-- Basic -->
+   <meta charset="utf-8" />
+   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+   <!-- Mobile Metas -->
+   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+   <!-- Site Metas -->
+   <meta name="keywords" content="" />
+   <meta name="description" content="" />
+   <meta name="author" content="" />
+   <link rel="shortcut icon" href="assets/images/favicon.png" type="">
+   
+ 
+   <title> Orthoc </title>
+ 
+   <!-- bootstrap core css -->
+   <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css" />
+ 
+   <!-- fonts style -->
+   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
+ 
+   <!--owl slider stylesheet -->
+   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
+ 
+   <!-- font awesome style -->
+   <link href="assets/css/font-awesome.min.css" rel="stylesheet" />
+ 
+   <!-- Custom styles for this template -->
+   <link href="assets/css/style.css" rel="stylesheet" />
+   <link href="assets/css/style2.css" rel="stylesheet" />
+   <!-- responsive style -->
+   <link href="assets/css/responsive.css" rel="stylesheet" />
+
+</head>
+
+<body class="sub_page">
+
+  <div class="hero_area">
+
+    <div class="hero_bg_box">
+      <img src="images/hero-bg.png" alt="">
+    </div>
+
+    <!-- header section strats -->
+    <header class="header_section">
+      <div class="container">
+        <nav class="navbar navbar-expand-lg custom_nav-container ">
+          <a class="navbar-brand" href="index.html">
+            <span>
+              Orthoc
+            </span>
+          </a>
+
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class=""> </span>
+          </button>
+
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" href="index.html">Home </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="about.html"> About</a>
+              </li>
+              <li class="nav-item active">
+                <a class="nav-link" href="penyakit.html">Penyakit</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="obat.html">Obat <span class="sr-only">(current)</span></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="doctors.html">Dokter</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="contact.html">Contact Us</a>
+              </li>
+              <form class="form-inline">
+                <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit">
+                  <i class="fa fa-search" aria-hidden="true"></i>
+                </button>
+              </form>
+            </ul>
+          </div>
+        </nav>
+      </div>
+    </header>
+    <!-- end header section -->
+  </div>
+
+  <!-- department section -->
+
+  <section class="department_section layout_padding">
+    <div class="department_container">
+      <div class="container">
+        <div class="card shadow mb-5" style="border-radius: 8px;">
+            <div class="card-body">
+                <img src="assets/images/paracetamoll.png" alt="paracetamol" width="400px" style="display: block; margin: auto;">
+                <h1 class="text-center">Paracetamol (Acetaminophen)</h1>
+                <p style="text-align: justify;">Paracetamol adalah obat untuk meredakan demam dan nyeri ringan hingga sedang, misalnya sakit kepala, nyeri haid, atau pegal-pegal. Paracetamol atau acetaminophen tersedia dalam bentuk tablet, sirop, tetes, suppositoria, dan infus.</p>
+                    <!-- <a href="pendaftaran.html" class="btn btn-dark py-3 px-5 animated slideInRight" style="border-radius: 8px;">Daftar Kegiatan</a> -->
+                    <p class="font-weight-bold">Peringatan Sebelum Menggunakan Paracetamol (Acetaminophen)</p>
+                    <p>Sebelum menggunakan paracetamol, Anda perlu memperhatikan beberapa hal berikut:</p>
+                    <ul>
+                    <li>Jangan menggunakan paracetamol jika Anda alergi terhadap obat ini. Beri tahu dokter tentang riwayat alergi yang Anda miliki.</li>
+                    <li>Konsultasikan dengan dokter mengenai penggunaan paracetamol jika Anda menderita penyakit liver, seperti sirosis atau hepatitis, penyakit ginjal, atau kecanduan alkohol.</li>
+                    <li>Beberapa produk paracetamol sirup mungkin mengandung aspartame. Perhatikan kandungan produk paracetamol yang akan dikonsumsi, terutama jika Anda menderita diabetes atau fenilketonuria.</li>
+                    <li>Konsultasikan terlebih dahulu dengan dokter mengenai penggunaan paracetamol sebelum Anda memberikan paracetamol kepada anak usia di bawah 2 tahun.</li>
+                    <li>Konsultasikan ke dokter mengenai penggunaan paracetamol jika Anda sedang mengonsumsi suplemen, produk herbal, atau obat tertentu, untuk mengantisipasi terjadinya interaksi antarobat.</li>
+                    <li>Konsultasikan dengan dokter perihal konsumsi obat penurun demam dan antinyeri, seperti paracetamol, jika Anda sedang hamil, menyusui, atau merencanakan kehamilan.</li>
+                    <li>Segera temui dokter jika terjadi reaksi alergi obat, efek samping yang serius, atau overdosis, setelah menggunakan paracetamol.</li>
+                  </ul>
+            </div>
+
+            <div class="slide-container swiper">
+              <div class="slide-content">
+                  <div class="card-wrapper swiper-wrapper">
+                      <div class="card swiper-slide">
+                          <div class="image-content">
+                              <span class="overlay"></span>
+                              <div class="card-image">
+                                  <!--<img src="images/profile1.jpg" alt="" class="card-img">-->
+                              </div>
+                          </div>
+                          <div class="card-content">
+                              <h2 class="name">David Dell</h2>
+                              <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+                              <button class="button">View More</button>
+                          </div>
+                      </div>
+                      <div class="card swiper-slide">
+                          <div class="image-content">
+                              <span class="overlay"></span>
+                              <div class="card-image">
+                                  <!--<img src="images/profile2.jpg" alt="" class="card-img">-->
+                              </div>
+                          </div>
+                          <div class="card-content">
+                              <h2 class="name">David Dell</h2>
+                              <p class="description">The lorem text the section that contains header with having open functionality. Lorem dolor sit amet consectetur adipisicing elit.</p>
+                              <button class="button">View More</button>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="swiper-button-next swiper-navBtn"></div>
+              <div class="swiper-button-prev swiper-navBtn"></div>
+              <div class="swiper-pagination"></div>
+          </div>
+          
+        </div>
+    </div>
+    </div>
+  </section>
+
+  <!-- end department section -->
+
+  <!-- footer section -->
+  <footer class="footer_section">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-lg-3 footer_col">
+          <div class="footer_contact">
+            <h4>
+              Reach at..
+            </h4>
+            <div class="contact_link_box">
+              <a href="">
+                <i class="fa fa-map-marker" aria-hidden="true"></i>
+                <span>
+                  Location
+                </span>
+              </a>
+              <a href="">
+                <i class="fa fa-phone" aria-hidden="true"></i>
+                <span>
+                  Call +01 1234567890
+                </span>
+              </a>
+              <a href="">
+                <i class="fa fa-envelope" aria-hidden="true"></i>
+                <span>
+                  demo@gmail.com
+                </span>
+              </a>
+            </div>
+          </div>
+          <div class="footer_social">
+            <a href="">
+              <i class="fa fa-facebook" aria-hidden="true"></i>
+            </a>
+            <a href="">
+              <i class="fa fa-twitter" aria-hidden="true"></i>
+            </a>
+            <a href="">
+              <i class="fa fa-linkedin" aria-hidden="true"></i>
+            </a>
+            <a href="">
+              <i class="fa fa-instagram" aria-hidden="true"></i>
+            </a>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-3 footer_col">
+          <div class="footer_detail">
+            <h4>
+              About
+            </h4>
+            <p>
+              Beatae provident nobis mollitia magnam voluptatum, unde dicta facilis minima veniam corporis laudantium alias tenetur eveniet illum reprehenderit fugit a delectus officiis blanditiis ea.
+            </p>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-2 mx-auto footer_col">
+          <div class="footer_link_box">
+            <h4>
+              Links
+            </h4>
+            <div class="footer_links">
+              <a class="active" href="index.html">
+                Home
+              </a>
+              <a class="" href="about.html">
+                About
+              </a>
+              <a class="" href="departments.html">
+                Departments
+              </a>
+              <a class="" href="doctors.html">
+                Doctors
+              </a>
+              <a class="" href="contact.html">
+                Contact Us
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-6 col-lg-3 footer_col ">
+          <h4>
+            Newsletter
+          </h4>
+       <form action="#">
+            <input type="email" placeholder="Enter email" />
+            <button type="submit">
+              Subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+      <div class="footer-info">
+        <p>
+          &copy; <span id="displayYear"></span> All Rights Reserved By Kelompok 2 Universitas Pamulang
+        </p>
+       
+      </div>  
+    </div>
+  </footer>
+  <!-- footer section -->
+
+ <!-- jQery -->
+ <script type="text/javascript" src="assets/js/jquery-3.4.1.min.js"></script>
+ <!-- popper js -->
+ <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
+ </script>
+ <!-- bootstrap js -->
+ <script type="text/javascript" src="assets/js/bootstrap.js"></script>
+ <!-- owl slider -->
+ <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
+ </script>
+ <!-- custom js -->
+ <script type="text/javascript" src="assets/js/custom.js"></script>
+ <!-- Google Map -->
+ <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh39n5U-4IoWpsVGUHWdqB6puEkhRLdmI&callback=myMap">
+ </script>
+ <!-- End Google Map -->
+
+</body>
+
+</html>
