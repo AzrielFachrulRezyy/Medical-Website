2c2af359de5ada57db5262e366bbabe3614302bf
Andri Firman Saputra <andrifirmansaputra1@gmail.com>
2023-05-31 18:46:57 +0700

    backup

--- dokter.php
+++ dokter.php
@@ -2,36 +2,8 @@
 <html>
 
 <head>
-  <!-- Basic -->
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <!-- Mobile Metas -->
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-  <!-- Site Metas -->
-  <meta name="keywords" content="" />
-  <meta name="description" content="" />
-  <meta name="author" content="" />
-  <link rel="shortcut icon" href="assets/images/favicon.png" type="">
-
-  <title> Orthoc </title>
-
-  <!-- bootstrap core css -->
-  <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css" />
-
-  <!-- fonts style -->
-  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
-
-  <!--owl slider stylesheet -->
-  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
-
-  <!-- font awesome style -->
-  <link href="assets/css/font-awesome.min.css" rel="stylesheet" />
-
-  <!-- Custom styles for this template -->
-  <link href="assets/css/style.css" rel="stylesheet" />
-  <!-- responsive style -->
-  <link href="assets/css/responsive.css" rel="stylesheet" />
-
+  <?php include_once 'include/head.php'; ?>
+  <title>Dokter</title>
 </head>
 
 <body class="sub_page">
@@ -43,49 +15,7 @@
     </div>
 
     <!-- header section strats -->
-    <header class="header_section">
-      <div class="container">
-        <nav class="navbar navbar-expand-lg custom_nav-container ">
-          <a class="navbar-brand" href="index.php">
-            <span>
-              Orthoc
-            </span>
-          </a>
-
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class=""> </span>
-          </button>
-
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="index.php">Home </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="about.php"> About</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="penyakit.php">Penyakit</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="obat.php">Obat</a>
-              </li>
-              <li class="nav-item active">
-                <a class="nav-link" href="dokter.php">dokter <span class="sr-only">(current)</span> </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="contact.php">Contact Us</a>
-              </li>
-              <form class="form-inline">
-                <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit">
-                  <i class="fa fa-search" aria-hidden="true"></i>
-                </button>
-              </form>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    </header>
+    <?php include_once 'include/header.php'; ?>
     <!-- end header section -->
   </div>
 
--- penyakit.php
+++ penyakit.php
@@ -1,37 +1,13 @@
+<?php 
+  require_once 'koneksi.php';
+  $penyakit = mysqli_query($koneksi, "SELECT * FROM penyakit ORDER BY nama_penyakit ASC");
+ ?>
 <!DOCTYPE html>
 <html>
 
 <head>
-   <!-- Basic -->
-   <meta charset="utf-8" />
-   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-   <!-- Mobile Metas -->
-   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-   <!-- Site Metas -->
-   <meta name="keywords" content="" />
-   <meta name="description" content="" />
-   <meta name="author" content="" />
-   <link rel="shortcut icon" href="assets/images/favicon.png" type="">
- 
-   <title> Orthoc </title>
- 
-   <!-- bootstrap core css -->
-   <link rel="stylesheet" type="text/css" href="assets/css/bootstrap.css" />
- 
-   <!-- fonts style -->
-   <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700;900&display=swap" rel="stylesheet">
- 
-   <!--owl slider stylesheet -->
-   <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/assets/owl.carousel.min.css" />
- 
-   <!-- font awesome style -->
-   <link href="assets/css/font-awesome.min.css" rel="stylesheet" />
- 
-   <!-- Custom styles for this template -->
-   <link href="assets/css/style.css" rel="stylesheet" />
-   <!-- responsive style -->
-   <link href="assets/css/responsive.css" rel="stylesheet" />
-
+  <title>Penyakit</title>
+  <?php include_once 'include/head.php'; ?>
 </head>
 
 <body class="sub_page">
@@ -43,49 +19,7 @@
     </div>
 
     <!-- header section strats -->
-    <header class="header_section">
-      <div class="container">
-        <nav class="navbar navbar-expand-lg custom_nav-container ">
-          <a class="navbar-brand" href="index.php">
-            <span>
-              Orthoc
-            </span>
-          </a>
-
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class=""> </span>
-          </button>
-
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav">
-              <li class="nav-item">
-                <a class="nav-link" href="index.php">Home </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="about.php"> About</a>
-              </li>
-              <li class="nav-item active">
-                <a class="nav-link" href="penyakit.php">Penyakit <span class="sr-only">(current)</span> </a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="obat.php">Obat</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="dokter.php">Dokter</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="contact.php">Contact Us</a>
-              </li>
-              <form class="form-inline">
-                <button class="btn  my-2 my-sm-0 nav_search-btn" type="submit">
-                  <i class="fa fa-search" aria-hidden="true"></i>
-                </button>
-              </form>
-            </ul>
-          </div>
-        </nav>
-      </div>
-    </header>
+    <?php include_once 'include/header.php'; ?>
     <!-- end header section -->
   </div>
 
@@ -98,263 +32,36 @@
           <h2>
             Info Penyakit
           </h2>
-          <p>
-            Asperiores sunt consectetur impedit nulla molestiae delectus repellat laborum dolores doloremque accusantium
-          </p>
         </div>
         <div class="row">
-          <div class="col-md-3">
-            <div class="box ">
-              <div class="img-box">
-                <img src="assets/images/penyakit.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Cardiology
-                </h5>
-                <p>
-                  fact that a reader will be distracted by the readable page when looking at its layout.
-                </p>
-                <a href="" class="btn btn-success text-white">Read More</a>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-3">
-            <div class="box ">
-              <div class="img-box">
-                <img src="assets/images/penyakit.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Diagnosis
-                </h5>
-                <p>
-                  fact that a reader will be distracted by the readable page when looking at its layout.
-                </p>
-                <a href="" class="btn btn-success text-white">Read More</a>
+          <?php foreach ($penyakit as $data_penyakit): ?>
+            <div class="col-md-3">
+              <div class="box ">
+                <div class="img-box">
+                  <img src="assets/images/penyakit.png" alt="">
+                </div>
+                <div class="detail-box">
+                  <h5>
+                    <?= $data_penyakit['nama_penyakit']; ?>
+                  </h5>
+                  <p>
+                    <?= htmlspecialchars_decode(html_entity_decode($data_penyakit['deskripsi_penyakit'])); ?>
+                  </p>
+                  <a href="" class="btn btn-success text-white">Read More</a>
+                </div>
               </div>
             </div>
-          </div>
-          <div class="col-md-3">
-            <div class="box ">
-              <div class="img-box">
-                <img src="assets/images/penyakit.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Surgery
-                </h5>
-                <p>
-                  fact that a reader will be distracted by the readable page when looking at its layout.
-                </p>
-                <a href="" class="btn btn-success text-white">Read More</a>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-3">
-            <div class="box ">
-              <div class="img-box">
-                <img src="assets/images/penyakit.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  First Aid
-                </h5>
-                <p>
-                  fact that a reader will be distracted by the readable page when looking at its layout.
-                </p>
-                <a href="" class="btn btn-success text-white">Read More</a>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-3">
-            <div class="box ">
-              <div class="img-box">
-                <img src="assets/images/penyakit.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Cardiology
-                </h5>
-                <p>
-                  fact that a reader will be distracted by the readable page when looking at its layout.
-                </p>
-                <a href="" class="btn btn-success text-white">Read More</a>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-3">
-            <div class="box ">
-              <div class="img-box">
-                <img src="assets/images/penyakit.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Diagnosis
-                </h5>
-                <p>
-                  fact that a reader will be distracted by the readable page when looking at its layout.
-                </p>
-                <a href="" class="btn btn-success text-white">Read More</a>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-3">
-            <div class="box ">
-              <div class="img-box">
-                <img src="assets/images/penyakit.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  Surgery
-                </h5>
-                <p>
-                  fact that a reader will be distracted by the readable page when looking at its layout.
-                </p>
-                <a href="" class="btn btn-success text-white">Read More</a>
-              </div>
-            </div>
-          </div>
-          <div class="col-md-3">
-            <div class="box">
-              <div class="img-box">
-                <img src="assets/images/penyakit.png" alt="">
-              </div>
-              <div class="detail-box">
-                <h5>
-                  First Aid
-                </h5>
-                <p>
-                  fact that a reader will be distracted by the readable page when looking at its layout.
-                </p>
-                <a href="" class="btn btn-success text-white">Read More</a>
-              </div>
-            </div>
-          </div>
+          <?php endforeach ?>
         </div>
       </div>
     </div>
   </section>
 
   <!-- end department section -->
-
-  <!-- footer section -->
-  <footer class="footer_section">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-6 col-lg-3 footer_col">
-          <div class="footer_contact">
-            <h4>
-              Reach at..
-            </h4>
-            <div class="contact_link_box">
-              <a href="">
-                <i class="fa fa-map-marker" aria-hidden="true"></i>
-                <span>
-                  Location
-                </span>
-              </a>
-              <a href="">
-                <i class="fa fa-phone" aria-hidden="true"></i>
-                <span>
-                  Call +01 1234567890
-                </span>
-              </a>
-              <a href="">
-                <i class="fa fa-envelope" aria-hidden="true"></i>
-                <span>
-                  demo@gmail.com
-                </span>
-              </a>
-            </div>
-          </div>
-          <div class="footer_social">
-            <a href="">
-              <i class="fa fa-facebook" aria-hidden="true"></i>
-            </a>
-            <a href="">
-              <i class="fa fa-twitter" aria-hidden="true"></i>
-            </a>
-            <a href="">
-              <i class="fa fa-linkedin" aria-hidden="true"></i>
-            </a>
-            <a href="">
-              <i class="fa fa-instagram" aria-hidden="true"></i>
-            </a>
-          </div>
-        </div>
-        <div class="col-md-6 col-lg-3 footer_col">
-          <div class="footer_detail">
-            <h4>
-              About
-            </h4>
-            <p>
-              Beatae provident nobis mollitia magnam voluptatum, unde dicta facilis minima veniam corporis laudantium alias tenetur eveniet illum reprehenderit fugit a delectus officiis blanditiis ea.
-            </p>
-          </div>
-        </div>
-        <div class="col-md-6 col-lg-2 mx-auto footer_col">
-          <div class="footer_link_box">
-            <h4>
-              Links
-            </h4>
-            <div class="footer_links">
-              <a class="active" href="index.php">
-                Home
-              </a>
-              <a class="" href="about.php">
-                About
-              </a>
-              <a class="" href="departments.php">
-                Departments
-              </a>
-              <a class="" href="dokter.php">
-                dokter
-              </a>
-              <a class="" href="contact.php">
-                Contact Us
-              </a>
-            </div>
-          </div>
-        </div>
-        <div class="col-md-6 col-lg-3 footer_col ">
-          <h4>
-            Newsletter
-          </h4>
-       <form action="#">
-            <input type="email" placeholder="Enter email" />
-            <button type="submit">
-              Subscribe
-            </button>
-          </form>
-        </div>
-      </div>
-      <div class="footer-info">
-        <p>
-          &copy; <span id="displayYear"></span> All Rights Reserved By Kelompok 2 Universitas Pamulang
-        </p>
-       
-      </div>  
-    </div>
-  </footer>
+  <?php include_once 'include/footer.php' ?>
   <!-- footer section -->
 
- <!-- jQery -->
- <script type="text/javascript" src="assets/js/jquery-3.4.1.min.js"></script>
- <!-- popper js -->
- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
- </script>
- <!-- bootstrap js -->
- <script type="text/javascript" src="assets/js/bootstrap.js"></script>
- <!-- owl slider -->
- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
- </script>
- <!-- custom js -->
- <script type="text/javascript" src="assets/js/custom.js"></script>
- <!-- Google Map -->
- <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCh39n5U-4IoWpsVGUHWdqB6puEkhRLdmI&callback=myMap">
- </script>
- <!-- End Google Map -->
+  <?php include_once 'include/script.php'; ?>
 
 </body>
 
